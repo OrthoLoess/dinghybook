974ac105e8397cd9c397b3baaa8667fff111ce60
OrthoLoess <ortho@ratship.net>
2023-10-25 20:48:00 +0100

    working implementation of flasker tutorial

--- booker/__init__.py
+++ booker/__init__.py
@@ -27,12 +27,16 @@ def create_app(test_config=None):
     #a simple page that says hello
     @app.route('/hello')
     def hello():
-        return f'Hello, World! - {app.instance_path}'
+        return f'Hello, World!'
 
     from . import db
     db.init_app(app)
 
     from . import auth
     app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
